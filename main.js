d5c0fd80330102a0b8f2ea611bdc250fe1b2ab28
rzhvlad <rzhvlad@gmail.com>
2025-01-03 11:41:08 +0000

    test

---
+++ main.js
@@ -0,0 +1,11 @@
+let array = [11, 0, -32, 2, 15, 67, 2, -4, 2, 74, 34];
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] >= 15) {
+    console.log(array[i]);
+  }
+}
+
+for (let num=0; i < array.length; i++) {
+    
+}
