692c9c5290f3bb0bcb073947b472ef908ce24c7b
rzhvlad <rzhvlad@gmail.com>
2025-01-04 22:35:35 +0000

    first task

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-let array = [11, 0, -32, 2, 15, 67, 2, -4, 2, 74, 34];
-
-for (let i = 0; i < array.length; i++) {
-  if (array[i] >= 15) {
-    console.log(array[i]);
-  }
-}
-
-for (let num=0; i < array.length; i++) {
-    
-}
+let number = prompt("Введите число:");
+let tenPercent = number * 0.1;
+console.log("10% от введенного числа:", tenPercent);
