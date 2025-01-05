c824a88dc87c42875b901fee8a39de71135ab2eb
rzhvlad <rzhvlad@gmail.com>
2025-01-05 22:57:05 +0000

    90%

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 //1
-let number = prompt("Введите число:");
-let tenPercent = number * 0.1;
-console.log("10% от введенного числа:", tenPercent);
+let inputNumber1 = +prompt("Введите число:");
+let tenPercent1 = inputNumber1 * 0.1;
+console.log("10% от введенного числа:", tenPercent1);
+
 //2
-let num1 = prompt("Введите первое число:");
-let num2 = prompt("Введите второе число:");
-let minNumber = Math.min(Number(num1), Number(num2));
-console.log("Наименьшее число:", minNumber);
-//3
-let number = prompt("Введите число:");
-if (number < 100) {
-    console.log("Число меньше 100");
-} else if (number > 100) {
-    console.log("Число больше 100");
+let inputNumber2a = +prompt("Введите первое число:");
+let inputNumber2b = +prompt("Введите второе число:");
+let smallerNumber;
+if (inputNumber2a < inputNumber2b) {
+    smallerNumber = inputNumber2a;
 } else {
-    console.log("Число равно 100");
+    smallerNumber = inputNumber2b;
 }
-//4
-let name = prompt("Введите ваше имя:");
-let age = prompt("Введите ваш возраст:");
-if (age >= 18) {
-    console.log("Hello, " + name);
+console.log("Наименьшее число:", smallerNumber);
+
+// 3
+let inputNumber3 = +prompt("Введите число (Task 3):");
+if (inputNumber3 < 100) {
+    console.log("Число меньше 100 (Task 3)");
+} else if (inputNumber3 > 100) {
+    console.log("Число больше 100 (Task 3)");
 } else {
-    console.log("Hi, " + name);
+    console.log("Число равно 100 (Task 3)");
 }
 
+// 4
+let userName4 = prompt("Введите ваше имя (Task 4):");
+let userAge4 = +prompt("Введите ваш возраст (Task 4):");
+if (userAge4 >= 18) {
+    console.log("Hello, " + userName4);
+} else {
+    console.log("Hi, " + userName4);
+}
